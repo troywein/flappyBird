107282c8733328e7c225289dacdea350c025c7a2
troy_weingart <git-commit-address>
2020-08-28 09:50:49 -0600

    first commit

---
+++ lsn07Example1.c
@@ -0,0 +1,33 @@
+/** lsn07Example1.c
+* ===========================================================
+* Name: CS210
+* Section:
+* Project: #define Example
+* Purpose: Example to demonstrate concepts.
+* Documentation:  None
+* ===========================================================
+*/
+#include <stdio.h>
+
+#define DEBUG 1 // 0 to turn off debugging
+
+int main () {
+
+    int sum = 0;
+    int numIters = 10;
+
+    // a loop that computes sum of first
+    // numIters integers
+    for(int i = 1; i <= numIters; i++) {
+        sum = sum + i;
+        if (DEBUG) {
+            printf("current sum = %d, current integer = %d\n", sum, i);
+        }
+    }
+
+    printf("sum = %d\n", sum);
+    printf("calculated sum = %d\n", (numIters * (numIters + 1)) / 2);
+
+}
+
+
